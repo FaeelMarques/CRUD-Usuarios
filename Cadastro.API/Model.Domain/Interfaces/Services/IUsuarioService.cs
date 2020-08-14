e97244d7423698ecf080a563d30fe2cd16d2cdf3
Rafael Marques Taranto <faeel.marques@gmail.com>
2020-08-13 23:32:34 -0300

    Adicionado entidade base e usuário, service base e usuário

---
+++ Cadastro.API/Model.Domain/Interfaces/Services/IUsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using Model.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Domain.Interfaces.Services
+{
+    public interface IUsuarioService : IServiceBase<Usuario>
+    {
+    }
+}
